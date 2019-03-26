562c8799e942554762245d4b252c584a109bfbbc
xian-wen <xianwen_yin@163.com>
2019-03-26 20:16:44 +0800

    merge two sorted lists

---
+++ C++/21.merge-two-sorted-lists.cpp
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode id=21 lang=cpp
+ *
+ * [21] Merge Two Sorted Lists
+ *
+ * https://leetcode.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (46.09%)
+ * Total Accepted:    534.3K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * Merge two sorted linked lists and return it as a new list. The new list
+ * should be made by splicing together the nodes of the first two lists.
+ * 
+ * Example:
+ * 
+ * Input: 1->2->4, 1->3->4
+ * Output: 1->1->2->3->4->4
+ * 
+ * 
+ */
+/* *
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+/* struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+}; */
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (l1 == NULL) return l2;
+        if (l2 == NULL) return l1;
+
+        // 确定头指针
+        ListNode* head = NULL; 
+        if (l1->val < l2->val) {
+            head = l1;
+            l1 = l1->next;
+        } else {
+            head = l2;
+            l2 = l2->next;
+        }
+        
+        ListNode* cur = head; // 当前结点为头结点
+        while (l1 != NULL && l2 != NULL) {
+            if (l1->val < l2->val) {
+                cur->next = l1;
+                l1 = l1->next;
+            } else {
+                cur->next = l2;
+                l2 = l2->next;
+            }
+            cur = cur->next;
+        }
+
+        // 链表非空，直接链上即可
+        if (l1 != NULL) cur->next = l1;
+        if (l2 != NULL) cur->next = l2;
+
+        return head;
+    }
+};
+
