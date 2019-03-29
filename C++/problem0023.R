fb73e4cf4d0ad78ff1c341287931a6f8903094b2
Vincent <35673838+xian-wen@users.noreply.github.com>
2019-03-29 11:08:00 +0800

    Delete problem0023.R

--- C++/problem0023.R
+++
@@ -1,9 +0,0 @@
-superhero_kills <- c("superman kills 1030 enemies.",
-                     "wonderwoman kills 206 enemies.", 
-                     "aquaman kills 32 enemies.",
-                     "cyborg kills 17 enemies.", 
-                     "batman kills 4 enemies.", 
-                     "the flash kills 0 enemies.")
-
-# 替换数字为X
-sub("([0-9]+)", "X", superhero_kills)
