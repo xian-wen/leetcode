cf6f8ef9e1cef0d6ef15563013d7d71c8297c3a0
xian-wen <xianwen_yin@163.com>
2019-04-05 18:47:21 +0800

    plus one

---
+++ Java/66.plus-one.java
@@ -0,0 +1,64 @@
+import java.util.Collection;
+
+/*
+ * @lc app=leetcode id=66 lang=java
+ *
+ * [66] Plus One
+ *
+ * https://leetcode.com/problems/plus-one/description/
+ *
+ * algorithms
+ * Easy (40.78%)
+ * Total Accepted:    369.7K
+ * Total Submissions: 903.9K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a non-empty array of digits representing a non-negative integer, plus
+ * one to the integer.
+ * 
+ * The digits are stored such that the most significant digit is at the head of
+ * the list, and each element in the array contain a single digit.
+ * 
+ * You may assume the integer does not contain any leading zero, except the
+ * number 0 itself.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,2,3]
+ * Output: [1,2,4]
+ * Explanation: The array represents the integer 123.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [4,3,2,1]
+ * Output: [4,3,2,2]
+ * Explanation: The array represents the integer 4321.
+ * 
+ */
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                break;
+            } else {
+                digits[i] = 0;
+            }
+        }
+
+        // the highest bit equals 0 only if all the other bits are 9
+        // new an array, default values are 0, only need to assign the first bit to 1
+        // eg: 1 0000 = 9999 + 1
+        if (digits[0] == 0) {
+            int[] res = new int[digits.length + 1];
+            res[0] = 1;
+            return res;
+        }
+
+        return digits;
+    }
+}
+
