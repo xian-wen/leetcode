e084a6297201e677eab71e2f2ebcb1058fa83faa
xian-wen <xianwen_yin@163.com>
2022-07-05 13:27:58 +0800

    Remove unused import

--- java/347.top-k-frequent-elements.java
+++ java/347.top-k-frequent-elements.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
