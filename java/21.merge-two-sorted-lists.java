23c407cabf238053485f40400042eb4df8cd9076
xian-wen <xianwen_yin@163.com>
2022-09-05 17:32:57 +0800

    Modify the iterative solution with a dummy head node

--- java/21.merge-two-sorted-lists.java
+++ java/21.merge-two-sorted-lists.java
@@ -29,56 +29,44 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+// Iterative:
+// class Solution {
+//     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+//         ListNode head = new ListNode(-1), p = head;
+//         while (l1 != null && l2 != null) {
+//             if (l1.val <= l2.val) {
+//                 p.next = l1;
+//                 l1 = l1.next;
+//             } else {
+//                 p.next = l2;
+//                 l2 = l2.next;
+//             }
+//             p = p.next;
+//         }
 
-/* 
+//         p.next = l1 == null ? l2 : l1;
+//         return head.next;
+//     }
+// }
+
+// Recursive:
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) return l2;
-        if (l2 == null) return l1;
-
-        // find the head node;
-        ListNode head = null;
-        if (l1.val < l2.val) {
-            head = l1;
-            l1 = l1.next;
-        } else {
-            head = l2;
-            l2 = l2.next;
-        }
-
-        ListNode cur = head; // head is the current node
-        while (l1 != null && l2 != null) {
-            if (l1.val < l2.val) {
-                cur.next = l1;
-                l1 = l1.next;
-            } else {
-                cur.next = l2;
-                l2 = l2.next;
-            }
-            cur = cur.next;
-        }
-
-        // link to cur directly if l1 or l2 not empty
-        if (l1 != null) cur.next = l1;
-        if (l2 != null) cur.next = l2;
-
-        return head;
-    }
-}
- */
-
-class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) return l2;
-        if (l2 == null) return l1;
-
-        if (l1.val < l2.val) {
-            l1.next = mergeTwoLists(l1.next, l2); // head node is l1
-            return l1;
-        } else {
-            l2.next = mergeTwoLists(l1, l2.next); // head node is l2
+        if (l1 == null) {
             return l2;
         }
+
+        if (l2 == null) {
+            return l1;
+        }
+
+        if (l1.val <= l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2;
     }
 }
 
