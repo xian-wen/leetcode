e7f052f4e60733c95b59dad127b5bd8384565c11
xian-wen <xianwen_yin@163.com>
2023-02-12 23:49:23 +0800

    Finish leetcode 155

---
+++ java/155.min-stack.java
@@ -0,0 +1,116 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/*
+ * @lc app=leetcode id=155 lang=java
+ *
+ * [155] Min Stack
+ *
+ * https://leetcode.com/problems/min-stack/description/
+ *
+ * algorithms
+ * Medium (52.17%)
+ * Likes:    10785
+ * Dislikes: 711
+ * Total Accepted:    1.2M
+ * Total Submissions: 2.3M
+ * Testcase Example:  '["MinStack","push","push","push","getMin","pop","top","getMin"]\n' +
+  '[[],[-2],[0],[-3],[],[],[],[]]'
+ *
+ * Design a stack that supports push, pop, top, and retrieving the minimum
+ * element in constant time.
+ * 
+ * Implement the MinStack class:
+ * 
+ * 
+ * MinStack() initializes the stack object.
+ * void push(int val) pushes the element val onto the stack.
+ * void pop() removes the element on the top of the stack.
+ * int top() gets the top element of the stack.
+ * int getMin() retrieves the minimum element in the stack.
+ * 
+ * 
+ * You must implement a solution with O(1) time complexity for each
+ * function.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input
+ * ["MinStack","push","push","push","getMin","pop","top","getMin"]
+ * [[],[-2],[0],[-3],[],[],[],[]]
+ * 
+ * Output
+ * [null,null,null,null,-3,null,0,-2]
+ * 
+ * Explanation
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin(); // return -3
+ * minStack.pop();
+ * minStack.top();    // return 0
+ * minStack.getMin(); // return -2
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * -2^31 <= val <= 2^31 - 1
+ * Methods pop, top and getMin operations will always be called on non-empty
+ * stacks.
+ * At most 3 * 10^4 calls will be made to push, pop, top, and getMin.
+ * 
+ * 
+ */
+
+// @lc code=start
+class MinStack {
+    private Deque<Integer> stack;
+    private int min;
+
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        min = Integer.MAX_VALUE;
+    }
+    
+    public void push(int val) {
+        if (val <= min) {
+            stack.push(min);
+            min = val;
+        }
+
+        stack.push(val);
+    }
+    
+    public void pop() {
+        if (stack.pop() == min) {
+            min = stack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    /**
+     * Also could create a Node inner class and make min as its field. 
+     */
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+// @lc code=end
+
