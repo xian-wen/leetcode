1df7d61c9aeff83d6d8f37a3c4d0e2a6bc852b29
xian-wen <xianwen_yin@163.com>
2023-02-15 17:48:57 +0800

    Finish leetcode 304

---
+++ java/304.range-sum-query-2-d-immutable.java
@@ -0,0 +1,116 @@
+/*
+ * @lc app=leetcode id=304 lang=java
+ *
+ * [304] Range Sum Query 2D - Immutable
+ *
+ * https://leetcode.com/problems/range-sum-query-2d-immutable/description/
+ *
+ * algorithms
+ * Medium (52.59%)
+ * Likes:    4251
+ * Dislikes: 317
+ * Total Accepted:    313.4K
+ * Total Submissions: 595.9K
+ * Testcase Example:  '["NumMatrix","sumRegion","sumRegion","sumRegion"]\n' +
+  '[[[[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]]],[2,1,4,3],[1,1,2,2],[1,2,2,4]]'
+ *
+ * Given a 2D matrix matrix, handle multiple queries of the following
+ * type:
+ * 
+ * 
+ * Calculate the sum of the elements of matrix inside the rectangle defined by
+ * its upper left corner (row1, col1) and lower right corner (row2, col2).
+ * 
+ * 
+ * Implement the NumMatrix class:
+ * 
+ * 
+ * NumMatrix(int[][] matrix) Initializes the object with the integer matrix
+ * matrix.
+ * int sumRegion(int row1, int col1, int row2, int col2) Returns the sum of the
+ * elements of matrix inside the rectangle defined by its upper left corner
+ * (row1, col1) and lower right corner (row2, col2).
+ * 
+ * 
+ * You must design an algorithm where sumRegion works on O(1) time
+ * complexity.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input
+ * ["NumMatrix", "sumRegion", "sumRegion", "sumRegion"]
+ * [[[[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1,
+ * 0, 3, 0, 5]]], [2, 1, 4, 3], [1, 1, 2, 2], [1, 2, 2, 4]]
+ * Output
+ * [null, 8, 11, 12]
+ * 
+ * Explanation
+ * NumMatrix numMatrix = new NumMatrix([[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1,
+ * 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]);
+ * numMatrix.sumRegion(2, 1, 4, 3); // return 8 (i.e sum of the red rectangle)
+ * numMatrix.sumRegion(1, 1, 2, 2); // return 11 (i.e sum of the green
+ * rectangle)
+ * numMatrix.sumRegion(1, 2, 2, 4); // return 12 (i.e sum of the blue
+ * rectangle)
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 200
+ * -10^4 <= matrix[i][j] <= 10^4
+ * 0 <= row1 <= row2 < m
+ * 0 <= col1 <= col2 < n
+ * At most 10^4 calls will be made to sumRegion.
+ * 
+ * 
+ */
+
+// @lc code=start
+class NumMatrix {
+    /**
+     * sum[r][c] = sumRegion(0, 0, r-1, c-1)
+     */
+    private int[][] sum;
+
+    /**
+     * Subproblem: 
+     * sum[r][c] = the sum of the region from (0, 0) to (r-1, c-1).
+     * 
+     * Recursive Relation:
+     * sum[r][c] = sum[r-1][c] + sum[r][c-1] - sum[r-1][c-1] + matrix[r-1][c-1]
+     * sum[0][c] = 0
+     * sum[r][0] = 0
+     * 
+     * Time complexity:
+     * O(MN)
+     */
+    public NumMatrix(int[][] matrix) {
+        int M = matrix.length, N = matrix[0].length;
+        sum = new int[M + 1][N + 1];
+        for (int r = 1; r <= M; ++r) {
+            for (int c = 1; c <= N; ++c) {
+                sum[r][c] = sum[r - 1][c] + sum[r][c - 1] - sum[r - 1][c - 1] 
+                        + matrix[r - 1][c - 1];
+            }
+        }
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return sum[row2 + 1][col2 + 1] - sum[row1][col2 + 1] 
+                - sum[row2 + 1][col1] + sum[row1][col1];
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
+// @lc code=end
+
