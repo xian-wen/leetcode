3928e2ab4367ef3bb725c46b95567db1dc3b30f7
xian-wen <xianwen_yin@163.com>
2023-02-17 01:28:31 +0800

    Finish leetcode 204

---
+++ java/204.count-primes.java
@@ -0,0 +1,79 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode id=204 lang=java
+ *
+ * [204] Count Primes
+ *
+ * https://leetcode.com/problems/count-primes/description/
+ *
+ * algorithms
+ * Medium (33.07%)
+ * Likes:    6423
+ * Dislikes: 1228
+ * Total Accepted:    705.5K
+ * Total Submissions: 2.1M
+ * Testcase Example:  '10'
+ *
+ * Given an integer n, return the number of prime numbers that are strictly
+ * less than n.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: n = 10
+ * Output: 4
+ * Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: n = 0
+ * Output: 0
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: n = 1
+ * Output: 0
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 0 <= n <= 5 * 10^6
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    /**
+     * Sieve of Eratosthenes:
+     * https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+     */
+    public int countPrimes(int n) {
+        boolean[] sieved = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < n; ++i) {
+            if (!sieved[i]) {
+                ++count;
+                // Exclude all multiples of i.
+                // Note: when n = 5e6, j = 2.5e13 < 0 (2's complete) < n,
+                // out of bounds of sieved!!!
+                for (long j = (long) i * i; j < n; j += i) {
+                    sieved[(int) j] = true;
+                }
+            }
+        }
+        return count;
+    }
+}
+// @lc code=end
+
