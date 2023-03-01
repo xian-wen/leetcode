ad693b4cb7cd9ee1cca61d8ae2aaf7a81c691aeb
xian-wen <xianwen_yin@163.com>
2023-03-01 20:48:18 +0800

    Finish leetcode 312

---
+++ java/312.burst-balloons.java
@@ -0,0 +1,141 @@
+/*
+ * @lc app=leetcode id=312 lang=java
+ *
+ * [312] Burst Balloons
+ *
+ * https://leetcode.com/problems/burst-balloons/description/
+ *
+ * algorithms
+ * Hard (56.91%)
+ * Likes:    7448
+ * Dislikes: 189
+ * Total Accepted:    221.3K
+ * Total Submissions: 388.6K
+ * Testcase Example:  '[3,1,5,8]'
+ *
+ * You are given n balloons, indexed from 0 to n - 1. Each balloon is painted
+ * with a number on it represented by an array nums. You are asked to burst all
+ * the balloons.
+ * 
+ * If you burst the i^th balloon, you will get nums[i - 1] * nums[i] * nums[i +
+ * 1] coins. If i - 1 or i + 1 goes out of bounds of the array, then treat it
+ * as if there is a balloon with a 1 painted on it.
+ * 
+ * Return the maximum coins you can collect by bursting the balloons wisely.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [3,1,5,8]
+ * Output: 167
+ * Explanation:
+ * nums = [3,1,5,8] --> [3,5,8] --> [3,8] --> [8] --> []
+ * coins =  3*1*5    +   3*5*8   +  1*3*8  + 1*8*1 = 167
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [1,5]
+ * Output: 10
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * n == nums.length
+ * 1 <= n <= 300
+ * 0 <= nums[i] <= 100
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    // private int[] balloons;
+    // private int[][] memo;
+
+    // /**
+    //  * Solution 1: Divide and Conquer + Memoization
+    //  */
+    // public int maxCoins(int[] nums) {
+    //     balloons = new int[nums.length + 2];
+    //     int n = 0;
+    //     balloons[n++] = 1;
+    //     for (int num : nums) {
+    //         if (num > 0) {
+    //             balloons[n++] = num;
+    //         }
+    //     }
+    //     balloons[n++] = 1;
+
+    //     memo = new int[n][n];
+    //     return burstBalloons(0, n - 1);
+    // }
+
+    // private int burstBalloons(int left, int right) {
+    //     if (left + 1 == right) {
+    //         return 0;
+    //     }
+
+    //     if (memo[left][right] > 0) {
+    //         return memo[left][right];
+    //     }
+
+    //     int maxCoins = 0;
+    //     for (int i = left + 1; i < right; ++i) {
+    //         int coins = balloons[left] * balloons[i] * balloons[right];
+    //         int leftCoins = burstBalloons(left, i);
+    //         int rightCoins = burstBalloons(i, right);
+    //         maxCoins = Math.max(maxCoins, coins + leftCoins + rightCoins);
+    //     }
+
+    //     memo[left][right] = maxCoins;
+    //     return maxCoins;
+    // }
+
+    /**
+     * Solution 2: Dynamic programming
+     * 
+     * Similar to Chain Matrix Multiply.
+     * 
+     * Subproblem:
+     * coins[i][j] = the maximum number of coins achievable by bursting 
+     * balloons[(i+1)..(j+1)].
+     * 
+     * Recursive relation:
+     * coins[i][j] = max{balloons[i] * balloons[k] * balloons[j] + coins[i][k] + coins[k][j]} where i < k < j
+     * coins[i][i+1] = 0
+     * 
+     * Time complexity:
+     * O(N^3)
+     */
+    public int maxCoins(int[] nums) {
+        int[] balloons = new int[nums.length + 2];
+        int n = 0;
+        balloons[n++] = 1;
+        for (int num : nums) {
+            if (num > 0) {
+                balloons[n++] = num;
+            }
+        }
+        balloons[n++] = 1;
+
+        int[][] coins = new int[n][n];
+        for (int s = 2; s < n; ++s) {  // The size of the window
+            for (int i = 0; i < n - s; ++i) {
+                int j = i + s;
+                coins[i][j] = 0;
+                for (int k = i + 1; k < j; ++k) {
+                    int curCoins = balloons[i] * balloons[k] * balloons[j]; 
+                    coins[i][j] = Math.max(coins[i][j], 
+                            curCoins + coins[i][k] + coins[k][j]);
+                }
+            }
+        }
+        return coins[0][n - 1];
+    }
+}
+// @lc code=end
+
