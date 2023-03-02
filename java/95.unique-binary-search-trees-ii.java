652d63a6e8918b1b30f072e98fb87d5087735d18
xian-wen <xianwen_yin@163.com>
2023-03-02 11:20:20 +0800

    Finish leetcode 95

---
+++ java/95.unique-binary-search-trees-ii.java
@@ -0,0 +1,95 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode id=95 lang=java
+ *
+ * [95] Unique Binary Search Trees II
+ *
+ * https://leetcode.com/problems/unique-binary-search-trees-ii/description/
+ *
+ * algorithms
+ * Medium (52.12%)
+ * Likes:    5719
+ * Dislikes: 375
+ * Total Accepted:    346.7K
+ * Total Submissions: 664.1K
+ * Testcase Example:  '3'
+ *
+ * Given an integer n, return all the structurally unique BST's (binary search
+ * trees), which has exactly n nodes of unique values from 1 to n. Return the
+ * answer in any order.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: n = 3
+ * Output:
+ * [[1,null,2,null,3],[1,null,3,2],[2,1,3],[3,1,null,null,2],[3,2,null,1]]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: n = 1
+ * Output: [[1]]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= n <= 8
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * Divide and conquer.
+     */
+    public List<TreeNode> generateTrees(int n) {
+        return generateTrees(1, n);
+    }
+
+    public List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> res = new ArrayList<>();
+        if (start > end) {
+            // Empty tree.
+            res.add(null);
+        }
+        
+        for (int i = start; i <= end; ++i) {
+            List<TreeNode> left = generateTrees(start, i - 1);
+            List<TreeNode> right = generateTrees(i + 1, end);
+            for (TreeNode l : left) {
+                for (TreeNode r : right) {
+                    TreeNode node = new TreeNode(i, l, r);
+                    res.add(node);
+                }
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
