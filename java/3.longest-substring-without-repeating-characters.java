86f0205d8d2b9bac30a486c79c84f2d360ce7bcb
xian-wen <xianwen_yin@163.com>
2023-03-04 18:27:06 +0800

    Finish leetcode 3

---
+++ java/3.longest-substring-without-repeating-characters.java
@@ -0,0 +1,97 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * @lc app=leetcode id=3 lang=java
+ *
+ * [3] Longest Substring Without Repeating Characters
+ *
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+ *
+ * algorithms
+ * Medium (33.82%)
+ * Likes:    32472
+ * Dislikes: 1416
+ * Total Accepted:    4.3M
+ * Total Submissions: 12.7M
+ * Testcase Example:  '"abcabcbb"'
+ *
+ * Given a string s, find the length of the longest substring without repeating
+ * characters.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "bbbbb"
+ * Output: 1
+ * Explanation: The answer is "b", with the length of 1.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: s = "pwwkew"
+ * Output: 3
+ * Explanation: The answer is "wke", with the length of 3.
+ * Notice that the answer must be a substring, "pwke" is a subsequence and not
+ * a substring.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 0 <= s.length <= 5 * 10^4
+ * s consists of English letters, digits, symbols and spaces.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    // /**
+    //  * Solution 1: i - lastIndexOf(s[i])
+    //  */
+    // public int lengthOfLongestSubstring(String s) {
+    //     if (s.length() <= 1) {
+    //         return s.length();
+    //     }
+
+    //     int N = s.length(), prev = -1, maxLen = 0;
+    //     for (int i = 1; i < N; ++i) {
+    //         String sub = s.substring(prev + 1, i);
+    //         int last = sub.lastIndexOf(s.charAt(i));
+    //         prev = last + prev + 1;
+    //         maxLen = Math.max(maxLen, i - prev);
+    //     }
+    //     return maxLen;
+    // }
+
+    /**
+     * Solution 2: HashSet
+     */
+    public int lengthOfLongestSubstring(String s) {
+        int N = s.length(), right = 0, left = 0, maxLen = 0;
+        Set<Character> win = new HashSet<>();
+        while (left < N) {
+            if (!win.contains(s.charAt(left))) {
+                win.add(s.charAt(left++));
+                maxLen = Math.max(maxLen, win.size());
+            } else {
+                win.remove(s.charAt(right++));
+            }
+        }
+        return maxLen;
+    }
+}
+// @lc code=end
+
