4f01c621c3d75f398857363af25bf2affa8b53fa
xian-wen <xianwen_yin@163.com>
2023-04-02 17:13:20 +0800

    Finish leetcode 145

---
+++ java/145.binary-tree-postorder-traversal.java
@@ -0,0 +1,126 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.List;
+
+/*
+ * @lc app=leetcode id=145 lang=java
+ *
+ * [145] Binary Tree Postorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+ *
+ * algorithms
+ * Easy (67.53%)
+ * Likes:    5771
+ * Dislikes: 169
+ * Total Accepted:    935.4K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given the root of a binary tree, return the postorder traversal of its
+ * nodes' values.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [1,null,2,3]
+ * Output: [3,2,1]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = []
+ * Output: []
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: root = [1]
+ * Output: [1]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of the nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ * 
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // /**
+    //  * Solution 1: Iteration
+    //  * 
+    //  * postorder: left -> right -> root, which is the reverse of
+    //  * mirrored preorder: root -> right -> left.
+    //  */
+    // public List<Integer> postorderTraversal(TreeNode root) {
+    //     List<Integer> res = new ArrayList<>();
+    //     if (root == null) {
+    //         return res;
+    //     }
+
+    //     Deque<TreeNode> stack = new ArrayDeque<>();
+    //     stack.push(root);
+    //     while (!stack.isEmpty()) {
+    //         TreeNode node = stack.pop();
+    //         res.add(node.val);
+    //         if (node.left != null) {
+    //             stack.push(node.left);
+    //         }
+
+    //         if (node.right != null) {
+    //             stack.push(node.right);
+    //         }
+    //     }
+
+    //     Collections.reverse(res);
+    //     return res;
+    // }
+
+    /**
+     * Solution 2: Recursion
+     */
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        postorder(root, res);
+        return res;
+    }
+
+    private void postorder(TreeNode node, List<Integer> res) {
+        if (node == null) {
+            return;
+        }
+
+        postorder(node.left, res);
+        postorder(node.right, res);
+        res.add(node.val);
+    }
+}
+// @lc code=end
+
