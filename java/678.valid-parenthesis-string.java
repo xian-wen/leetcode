0b794ff8fded1dd74c5f93aaa123afe3b65325d7
xian-wen <xianwen_yin@163.com>
2023-04-04 21:07:34 +0800

    Add solution with only count

--- java/678.valid-parenthesis-string.java
+++ java/678.valid-parenthesis-string.java
@@ -1,3 +1,6 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /*
  * @lc app=leetcode id=678 lang=java
  *
@@ -7,30 +10,91 @@
 // @lc code=start
 class Solution {
     // /**
-    //  * Solution 1
-    //  * 
-    //  * countMin: # '(' must be paired
-    //  * countMax: # '(' could be paired
-    //  * 
-    //  * countMin: count '*' as ')'
-    //  * countMax: count '*' as '('
-    //  * 
-    //  * countMin must not < 0:
-    //  * ')' should not match '(' after it,
-    //  * i.e., )( or *(
+    //  * Solution 1: Two Stacks
+    //  */
+    // public boolean checkValidString(String s) {
+    //     Deque<Integer> openStack = new ArrayDeque<>();
+    //     Deque<Integer> starStack = new ArrayDeque<>();
+    //     int N = s.length();
+    //     for (int i = 0; i < N; ++i) {
+    //         char c = s.charAt(i);
+    //         if (c == '(') {
+    //             openStack.push(i);
+    //         } else if (c == '*') {
+    //             starStack.push(i);
+    //         } else {  // )
+    //             if (openStack.isEmpty() && starStack.isEmpty()) {
+    //                 return false;
+    //             }
+
+    //             if (!openStack.isEmpty()) {
+    //                 openStack.pop();
+    //             } else {
+    //                 starStack.pop();
+    //             }
+    //         }
+    //     }
+
+    //     while (!openStack.isEmpty() && !starStack.isEmpty()) {
+    //         // '(' must be in front of '*',
+    //         // i.e., *( is invalid.
+    //         if (openStack.pop() > starStack.pop()) {
+    //             return false;
+    //         }
+    //     }
+    //     return openStack.isEmpty();
+    // }
+
+    /**
+     * Solution 2: countMin + countMax
+     * 
+     * countMin: # '(' must be paired
+     * countMax: # '(' could be paired
+     * 
+     * countMin: count '*' as ')'
+     * countMax: count '*' as '('
+     * 
+     * countMin must not < 0:
+     * ')' should not match '(' after it,
+     * i.e., )( or *(
+     */
+    public boolean checkValidString(String s) {
+        int countMin = 0, countMax = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                ++countMin;
+                ++countMax;
+            } else if (c == ')') {
+                --countMax;
+                countMin = Math.max(countMin - 1, 0);
+            } else {  // c = *
+                ++countMax;
+                countMin = Math.max(countMin - 1, 0);
+            }
+
+            if (countMax < 0) {
+                return false;
+            }
+        }
+        return countMin == 0;
+    }
+
+    // /**
+    //  * Solution 3: countMin + countMax (Simplified)
     //  */
     // public boolean checkValidString(String s) {
     //     int countMin = 0, countMax = 0;
     //     for (char c : s.toCharArray()) {
     //         if (c == '(') {
     //             ++countMin;
-    //             ++countMax;
-    //         } else if (c == ')') {
+    //         } else {  // ')' or '*'
+    //             countMin = Math.max(countMin - 1, 0);
+    //         }
+
+    //         if (c == ')') {
     //             --countMax;
-    //             countMin = Math.max(countMin - 1, 0);
-    //         } else {  // c = *
+    //         } else {  // '(' or '*'
     //             ++countMax;
-    //             countMin = Math.max(countMin - 1, 0);
     //         }
 
     //         if (countMax < 0) {
@@ -40,30 +104,38 @@ class Solution {
     //     return countMin == 0;
     // }
 
-    /**
-     * Solution 2: Simplified
-     */
-    public boolean checkValidString(String s) {
-        int countMin = 0, countMax = 0;
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                ++countMin;
-            } else {  // ')' or '*'
-                countMin = Math.max(countMin - 1, 0);
-            }
+    // /**
+    //  * Solution 4: Count
+    //  */
+    // public boolean checkValidString(String s) {
+    //     return checkValidString(s, '(');
+    // }
 
-            if (c == ')') {
-                --countMax;
-            } else {  // '(' or '*'
-                ++countMax;
-            }
+    // private boolean checkValidString(String s, char par) {
+    //     int N = s.length(), count = 0;
+    //     for (int i = 0; i < N; ++i) {
+    //         char c = s.charAt(i);
+    //         // count '*' as par.
+    //         if (c == par || c == '*') {
+    //             ++count;
+    //         } else {
+    //             --count;
+    //             if (count < 0) {
+    //                 return false;
+    //             }
+    //         }
+    //     }
 
-            if (countMax < 0) {
-                return false;
-            }
-        }
-        return countMin == 0;
-    }
+    //     if (count == 0) {
+    //         return true;
+    //     }
+
+    //     String reversed = new StringBuilder(s).reverse().toString();
+    //     if (par == '(') {
+    //         return checkValidString(reversed, ')');
+    //     }
+    //     return true;
+    // }
 }
 // @lc code=end
 
