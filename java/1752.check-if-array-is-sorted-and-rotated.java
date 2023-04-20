4a1085e05ab3f0cde59c0054ff75df6858423a7f
xian-wen <xianwen_yin@163.com>
2023-04-20 14:29:09 +0800

    Add one more solution

--- java/1752.check-if-array-is-sorted-and-rotated.java
+++ java/1752.check-if-array-is-sorted-and-rotated.java
@@ -6,31 +6,53 @@
 
 // @lc code=start
 class Solution {
+    // /**
+    //  * Solution 1
+    //  */
+    // public boolean check(int[] nums) {
+    //     int index = indexOfFirstDescending(nums);
+    //     int N = nums.length;
+    //     if (index == N) {
+    //         return true;
+    //     }
+
+    //     if (!isSuffixSorted(nums, index)) {
+    //         return false;
+    //     }
+    //     return nums[N - 1] <= nums[0]; 
+    // }
+
+    // private int indexOfFirstDescending(int[] nums) {
+    //     int N = nums.length, i = 0;
+    //     while (i < N - 1 && nums[i] <= nums[i + 1]) {
+    //         ++i;
+    //     }
+    //     return i + 1;
+    // }
+
+    // private boolean isSuffixSorted(int[] nums, int start) {
+    //     int N = nums.length;
+    //     for (int i = start + 1; i < N; ++i) {
+    //         if (nums[i - 1] > nums[i]) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+
+    /**
+     * Solution 2
+     * 
+     * For a valid rotated sorted array, at most one inverse is allowed.
+     */
     public boolean check(int[] nums) {
-        int index = indexOfFirstDescending(nums);
-        int N = nums.length;
-        if (index == N) {
-            return true;
-        }
+        int N = nums.length, inverse = 0;
+        for (int i = 0; i < N; ++i) {
+            if (nums[i] > nums[(i + 1) % N]) {
+                ++inverse;
+            }
 
-        if (!isSuffixSorted(nums, index)) {
-            return false;
-        }
-        return nums[N - 1] <= nums[0]; 
-    }
-
-    private int indexOfFirstDescending(int[] nums) {
-        int N = nums.length, i = 0;
-        while (i < N - 1 && nums[i] <= nums[i + 1]) {
-            ++i;
-        }
-        return i + 1;
-    }
-
-    private boolean isSuffixSorted(int[] nums, int start) {
-        int N = nums.length;
-        for (int i = start + 1; i < N; ++i) {
-            if (nums[i - 1] > nums[i]) {
+            if (inverse > 1) {
                 return false;
             }
         }
