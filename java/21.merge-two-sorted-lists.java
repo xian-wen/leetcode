ab162714778b6a2bee7be51c4a783fbc877a22d5
xian-wen <xianwen_yin@163.com>
2023-04-20 22:52:48 +0800

    Modify comments

--- java/21.merge-two-sorted-lists.java
+++ java/21.merge-two-sorted-lists.java
@@ -29,28 +29,30 @@
  *     ListNode(int x) { val = x; }
  * }
  */
-// Iterative:
-// class Solution {
-//     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-//         ListNode head = new ListNode(-1), p = head;
-//         while (l1 != null && l2 != null) {
-//             if (l1.val <= l2.val) {
-//                 p.next = l1;
-//                 l1 = l1.next;
-//             } else {
-//                 p.next = l2;
-//                 l2 = l2.next;
-//             }
-//             p = p.next;
-//         }
-
-//         p.next = l1 == null ? l2 : l1;
-//         return head.next;
-//     }
-// }
-
-// Recursive:
 class Solution {
+    // /**
+    //  * Solution 1: Iteration
+    //  */
+    // public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    //     ListNode head = new ListNode(-1), p = head;
+    //     while (l1 != null && l2 != null) {
+    //         if (l1.val <= l2.val) {
+    //             p.next = l1;
+    //             l1 = l1.next;
+    //         } else {
+    //             p.next = l2;
+    //             l2 = l2.next;
+    //         }
+    //         p = p.next;
+    //     }
+
+    //     p.next = l1 == null ? l2 : l1;
+    //     return head.next;
+    // }
+
+    /**
+     * Solution 2: Recursion 
+     */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null) {
             return l2;
@@ -69,4 +71,3 @@ class Solution {
         return l2;
     }
 }
-
