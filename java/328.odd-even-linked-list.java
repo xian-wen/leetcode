df27213b44c9d61fb8190844b79b1aa7f191c2d6
xian-wen <xianwen_yin@163.com>
2023-05-03 20:51:25 +0800

    Finish leetcode 328

---
+++ java/328.odd-even-linked-list.java
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=328 lang=java
+ *
+ * [328] Odd Even Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+
+        ListNode evenHead = head.next;
+        ListNode odd = head, even = evenHead;
+        while (even != null && even.next != null) {
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+        return head;
+    }
+
+    private String listToString(ListNode head) {
+        if (head.next == null) {
+            return String.valueOf(head.val);
+        }
+        return String.format("%d->%s", head.val, listToString(head.next));
+    }
+}
+// @lc code=end
+
